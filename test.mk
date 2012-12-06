31b54df74fc44d1f88cc267410c7ea75ce8739ba
Niels Thykier <niels@thykier.net>
2012-12-06 14:07:35 +0100

    Add check-campaign.py to test compaigns
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- test.mk
+++ test.mk
@@ -1,11 +1,14 @@
 #!/usr/bin/make -f
 
 LVL_EXT := .txt
+LSF_EXT := .lsf
+CAMPAIGNS := $(wildcard levels/*$(LSF_EXT))
 PLAYABLE_MAPS := $(wildcard levels/*$(LVL_EXT))
 SOLVABLE_TESTS := $(wildcard tests/solvable/*$(LVL_EXT))
 TIMEPARADOX_TESTS := $(wildcard tests/time-paradox/*$(LVL_EXT))
 
-check: $(SOLVABLE_TESTS) $(PLAYABLE_MAPS) $(TIMEPARADOX_TESTS)
+check: $(SOLVABLE_TESTS) $(PLAYABLE_MAPS) $(TIMEPARADOX_TESTS) $(CAMPAIGNS)
 	./check-level.py --solvable $(SOLVABLE_TESTS)
 	./check-level.py --solvable $(PLAYABLE_MAPS)
 	./check-level.py --test-time-paradox $(TIMEPARADOX_TESTS)
+	./check-campaign.py $(CAMPAIGNS)
