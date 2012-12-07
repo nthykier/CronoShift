ec4dfa72a921c8830e52989be7f7d3f11703e8d4
Niels Thykier <niels@thykier.net>
2012-12-07 20:38:11 +0100

    level: Remove unused import
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- chrono/model/level.py
+++ chrono/model/level.py
@@ -26,7 +26,6 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from collections import deque
 import functools
 from itertools import (imap, ifilter, chain, izip, takewhile, product,
                        starmap)
